66ff581505f03f461b6a7b3a7c040c3887020969
chinmay <chinmay@getrealhealth.com>
2022-03-28 16:40:55 +0530

    feat:search screen skelton

---
+++ src/app/search-videos/search-videos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-videos',
+  templateUrl: './search-videos.component.html',
+  styleUrls: ['./search-videos.component.scss']
+})
+export class SearchVideosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
